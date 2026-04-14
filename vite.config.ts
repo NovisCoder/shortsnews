bf765e7f1751ec3ab0da8431de9395a5e0e8c661
NovisCoder <noviscoder@gmail.com>
2026-04-14 22:31:04 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   root: path.resolve(__dirname, "client"),
-  base: "/"
+  base: "/",
   plugins: [
     react({
       jsxRuntime: "automatic",
