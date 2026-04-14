232008f1f2f545b5f604b8d9ea1d77745b85dbc5
NovisCoder <noviscoder@gmail.com>
2026-04-14 16:38:35 +0900

    Update GeneratePage.tsx

--- client/src/pages/GeneratePage.tsx
+++ client/src/pages/GeneratePage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { useHashLocation } from "wouter/use-hash-location";
 import { setSessionValue, getSessionValue } from "../lib/sessionStore";
